ec49f6c4d904e1f0749e0074f50ed76023bdc271
Fardin7864 <fardinhasan1888@gmail.com>
2024-03-11 17:47:59 +0600

    fix error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,26 @@
-import Image from "next/image";
+import Image  from "next/image";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" flex flex-col items-center mt-8">
-      <div className=" flex items-center gap-3">
+    <div className="flex flex-col items-center mt-8">
+      <div className="flex items-center gap-3">
         <Image
           src={"/logo.png"}
           width={65}
           height={65}
           alt="Logo"
-          className=" w-10 md:w-16"
+          className="w-10 md:w-16"
         />
-        <h3 className=" text-2xl md:text-4xl lg:text-6xl text-black font-extrabold logo">
+        <h3 className="text-2xl md:text-4xl lg:text-6xl text-black font-extrabold logo">
           BOAST
         </h3>
       </div>
-      <div className=" flex flex-col items-center my-5">
-        <p className=" text-justify lg:w-1/2">It is simple! Once you join Boast free membership, you can shop at any of our partner brands Then, share about your purchase on your social media channels, tagging the brand and get a cashback ranging from 10-100% depending open your social influencing power</p>
+      <div className="flex flex-col items-center my-5">
+        <p className="text-justify lg:w-1/2">It is simple! Once you join Boast free membership, you can shop at any of our partner brands. Then, share about your purchase on your social media channels, tagging the brand and get a cashback ranging from 10-100% depending on your social influencing power.</p>
       </div>
-      <div className=" w-full flex flex-col items-center">
-        <p> 2024 BOAST  All rights reserved  Made with </p>
+      <div className="w-full flex flex-col items-center">
+        <p>2024 BOAST All rights reserved. Made with &hearts;</p>
       </div>
     </div>
   );
